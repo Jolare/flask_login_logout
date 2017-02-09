f937dbb093e435a9e9c60521b48c6a3d39086ddc
johd <jolare.ky@gmail.com>
2017-02-09 21:50:28 +0100

    updated errors, added sqldump

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ mysql = MySQL()
 
 # MySQL Config
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = '3076056j'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'alsoasecret'
 app.config['MYSQL_DATABASE_DB'] = 'ToDoListe'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
